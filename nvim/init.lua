5ce1ac9463d8b10b80250cd4ccf24fad23007be4
Liza Chevalier <lizalc@pm.me>
2022-06-09 15:04:35 -0400

    fix(nvim): fix lsp startup status notification
    Spinner did not work properly due to notif_data.spinner never being set
    and from the notification data not being properly cleared.
    
    Status notification callback now wrapped in `pcall` as something about
    `null-ls` causes it to error. `pcall` silences the error.

--- nvim/init.lua
+++ nvim/init.lua
@@ -754,52 +754,58 @@ vim.api.nvim_create_autocmd("User", {
     group = "lsp_notify",
     desc = "LSP progress notification",
     callback = function()
-        local bufnr = vim.api.nvim_get_current_buf() -- get buffer number
-        for _, client in pairs(vim.lsp.get_active_clients()) do -- loop over each client to check notifications
-            if not client_notifs[bufnr] then -- create buffer specific notification table if not exists
-                client_notifs[bufnr] = {}
-            end
-            if not client_notifs[bufnr][client.id] then -- create buffers client  specific  notification table if not exists
-                client_notifs[bufnr][client.id] = {}
-            end
-            local notif_data = client_notifs[bufnr][client.id] -- set notif_data variable
-            local progress = nil
-            for _, progress_msg in pairs(client.messages.progress) do
-                progress = true -- expose if a progress exists
-                if not progress_msg.done then
-                    progress = progress_msg -- get clients first not done progress messages
-                    break
+        local cb = function()
+            local bufnr = vim.api.nvim_get_current_buf() -- get buffer number
+            for _, client in pairs(vim.lsp.get_active_clients()) do -- loop over each client to check notifications
+                if not client_notifs[bufnr] then -- create buffer specific notification table if not exists
+                    client_notifs[bufnr] = {}
                 end
-            end
-            if type(progress) == "table" and progress.percentage and progress.percentage ~= 0 then -- if there is a progress message
-                local notify_opts = {} -- define notification options
-                local new_msg = notif_data.notification == nil -- if it's a new message set different options
-                if new_msg then -- for new messages set a title, initialize icone and disable timeout
-                    notify_opts = {
-                        title = client.name .. (#progress.title > 0 and ": " .. progress.title or ""),
-                        icon = spinner_frames[1],
-                        timeout = false,
-                    }
-                else -- for existing messages just update the existing notification
-                    notify_opts = { replace = notif_data.notification }
+                if not client_notifs[bufnr][client.id] then -- create buffers client  specific  notification table if not exists
+                    client_notifs[bufnr][client.id] = {}
                 end
-                notif_data.notification = vim.notify(-- notify with percentage and message
-                    (progress.percentage and progress.percentage .. "%\t" or "") .. (progress.message or ""),
-                    "info",
-                    notify_opts
-                )
-                if new_msg then -- if it's a new message, start the update spinner background job
-                    update_spinner(notif_data)
+                local notif_data = client_notifs[bufnr][client.id] -- set notif_data variable
+                local progress = nil
+                for _, progress_msg in pairs(client.messages.progress) do
+                    progress = true -- expose if a progress exists
+                    if not progress_msg.done then
+                        progress = progress_msg -- get clients first not done progress messages
+                        break
+                    end
                 end
-            elseif progress and not vim.tbl_isempty(notif_data) then -- if there is finished progress and a notification, complete it
-                notif_data.notification = vim.notify(
-                    "Complete",
-                    "info",
-                    { icon = "", replace = notif_data.notification, timeout = 3000 }
-                )
-                notif_data = {} -- clear notification data
+                -- if there is a progress message
+                if type(progress) == "table" and progress.percentage and progress.percentage ~= 0 then
+                    local notify_opts
+                    local new_msg = notif_data.notification == nil -- if it's a new message set different options
+                    if new_msg then -- for new messages set a title, initialize icone and disable timeout
+                        notify_opts = {
+                            title = client.name .. (#progress.title > 0 and ": " .. progress.title or ""),
+                            icon = spinner_frames[1],
+                            timeout = false,
+                        }
+                    else -- for existing messages just update the existing notification
+                        notify_opts = { replace = notif_data.notification }
+                    end
+                    notif_data.notification = vim.notify(-- notify with percentage and message
+                        (progress.percentage and progress.percentage .. "%\t" or "") .. (progress.message or ""),
+                        "info",
+                        notify_opts
+                    )
+                    if new_msg then -- if it's a new message, start the update spinner background job
+                        notif_data.spinner = 1
+                        update_spinner(notif_data)
+                    end
+                    -- if there is finished progress and a notification, complete it
+                elseif progress and not vim.tbl_isempty(notif_data) then
+                    notif_data.notification = vim.notify(
+                        "Complete",
+                        "info",
+                        { icon = "", replace = notif_data.notification, timeout = 3000 }
+                    )
+                    notif_data.spinner = nil
+                end
             end
         end
+        pcall(cb) -- something about null-ls (?) causes the status notification function to throw.
     end,
 })
 
