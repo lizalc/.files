65e58de0c33af740c2efb9cdd551731e9eeff95a
Liza Chevalier <lizalc@pm.me>
2022-07-05 12:44:04 -0400

    style(nvim): init.lua formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,13 +74,13 @@ require("neogit").setup()
 local dap, dapui = require("dap"), require("dapui")
 dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 require("dap-go").setup()
 require("nvim-dap-virtual-text").setup()
