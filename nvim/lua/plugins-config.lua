36c1396c064fc14d62eb120fe3590f98b6b0a582
Liza Chevalier <lizalc@pm.me>
2022-11-05 22:16:26 -0400

    wip

--- nvim/lua/plugins-config.lua
+++ nvim/lua/plugins-config.lua
@@ -288,6 +288,19 @@ packer.startup({
 
 		use({ "glepnir/lspsaga.nvim" })
 
+		use({
+			"folke/noice.nvim",
+			event = "VimEnter",
+			config = function()
+				-- require("noice").setup()
+			end,
+			requires = {
+				"MunifTanjim/nui.nvim",
+				"rcarriga/nvim-notify",
+				"hrsh7th/nvim-cmp",
+			},
+		})
+
 		if packer_bootstrap then
 			packer.sync()
 		end
